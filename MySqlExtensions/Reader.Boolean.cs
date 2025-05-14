836defabee48734f52fa217ed1a7950cf493a0b7
7873930+GStefanowich <7873930+GStefanowich@users.noreply.github.com>
2025-05-14 19:49:33 -0400

    Added fallback 'bool' option version minor bump

--- MySqlExtensions/Reader.Boolean.cs
+++ MySqlExtensions/Reader.Boolean.cs
@@ -4,5 +4,14 @@ namespace TheElm.MySql {
     public static partial class Reader {
         public static bool GetBoolean( this MySqlDataReader reader, string table, string column )
             => reader.GetBoolean(reader.GetOrdinal(table, column));
+        
+        public static bool GetBoolean( this MySqlDataReader reader, string table, string column, bool defaultValue )
+            => reader.TryGetOrdinal(table, column, out int ordinal) ? reader.GetBoolean(ordinal) : defaultValue;
+        
+        public static bool GetBoolean( this MySqlDataReader reader, string column )
+            => reader.GetBoolean(reader.GetOrdinal(null, column));
+        
+        public static bool GetBoolean( this MySqlDataReader reader, string column, bool defaultValue )
+            => reader.TryGetOrdinal(column, out int ordinal) ? reader.GetBoolean(ordinal) : defaultValue;
     }
 }
