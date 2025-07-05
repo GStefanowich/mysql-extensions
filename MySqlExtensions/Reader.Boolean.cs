5c6cd9b0bf8720682ac8ffe1bc9dcae3b6ab2b58
7873930+GStefanowich <7873930+GStefanowich@users.noreply.github.com>
2025-07-05 19:33:20 -0400

    fix: Add missing IsDbNull check to GetBoolean when using a fallback value

--- MySqlExtensions/Reader.Boolean.cs
+++ MySqlExtensions/Reader.Boolean.cs
@@ -6,12 +6,12 @@ namespace TheElm.MySql {
             => reader.GetBoolean(reader.GetOrdinal(table, column));
         
         public static bool GetBoolean( this MySqlDataReader reader, string table, string column, bool defaultValue )
-            => reader.TryGetOrdinal(table, column, out int ordinal) ? reader.GetBoolean(ordinal) : defaultValue;
+            => reader.TryGetOrdinal(table, column, out int ordinal) && !reader.IsDBNull(ordinal) ? reader.GetBoolean(ordinal) : defaultValue;
         
         public static bool GetBoolean( this MySqlDataReader reader, string column )
             => reader.GetBoolean(reader.GetOrdinal(null, column));
         
         public static bool GetBoolean( this MySqlDataReader reader, string column, bool defaultValue )
-            => reader.TryGetOrdinal(column, out int ordinal) ? reader.GetBoolean(ordinal) : defaultValue;
+            => reader.TryGetOrdinal(column, out int ordinal) && !reader.IsDBNull(ordinal) ? reader.GetBoolean(ordinal) : defaultValue;
     }
 }
